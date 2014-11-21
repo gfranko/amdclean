feef30990d245891ab3455a29e9695ea11660a7a
Kirill Konshin <kirill.konshin@gmail.com>
2014-11-20 17:46:46 -0800

    Moved escodegen comments procedure to createAst.js from generateCode.js (after traversal comments may not be applied in some cases)

--- src/modules/createAst.js
+++ src/modules/createAst.js
@@ -14,7 +14,8 @@ define([
       options = amdclean.options,
       filePath = options.filePath,
       code = providedCode || options.code || (filePath ? utils.readFile(filePath) : ''),
-      esprimaOptions = options.esprima;
+      esprimaOptions = options.esprima,
+      escodegenOptions = options.escodegen;
 
     if (!code) {
       throw new Error(errorMsgs.emptyCode);
@@ -24,6 +25,13 @@ define([
       }
       var ast = esprima.parse(code, esprimaOptions);
       if (options.sourceMap) sourcemapToAst(ast, options.sourceMap);
+      // Check if both the esprima and escodegen comment options are set to true
+      if (esprimaOptions.comment === true && escodegenOptions.comment === true) {
+        try {
+          // Needed to keep source code comments when generating the code with escodegen
+          ast = escodegen.attachComments(ast, ast.comments, ast.tokens);
+        } catch (e) {}
+      }
       return ast;
     }
   };
